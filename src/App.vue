aa499103852f929ceaa7d2ffdb8c2b5f42587f06
Aintzane Goffard <152596650+A-Goffard@users.noreply.github.com>
2024-03-06 21:34:23 +0100

    empezando con vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- código html -->
+    <div>
+        <h1 v-bind:style="miColor">Hola {{miNombre}}, ¿qué tal estás hoy? Parece que tienes {{ edad - 10}} años</h1>
+        <!-- v-bind:style es lo mismo que :style -->
+        <button v-on:click="">Aceptar</button>
+        <!-- v-on:click es lo mismo que @click -->
+    </div>
+</template>
+
+
+<script setup>//setup es para que la exportación se haga automáticamente
+//codigo js
+const miNombre = "Aintzane";
+const edad = 25;
+const miColor = "color:rgb(184, 93, 196);font-size:3rem"
+</script>
+
+<style scoped>/* scoped es para que los estilos solo afecten a esta página */
+/* código CSS */
+
+</style>
